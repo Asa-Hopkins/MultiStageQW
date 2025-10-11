0fb3f4d01f05931b7a59d2e94a709ab946117094
Asa Hopkins <92234649+Asa-Hopkins@users.noreply.github.com>
2025-10-11 19:14:17 +0100

    Refactoring MultiQW.cpp
    I'm refactoring MultiQW to improve performance and readability. So far I've split the psi array into separate real and imaginary vectors so I can make better use of the fact that the coefficients of exp(ix) alternative between real and imaginary.

--- MultiQW.cpp
+++ MultiQW.cpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <random>
 #include <fstream>
-#include <cstdio> // for sprintf
 
 #define PI 3.1415926535897932384626
 
@@ -35,7 +34,6 @@ unsigned int log2(unsigned int v){
 
 
 //Taken from https://stackoverflow.com/questions/27229371/inverse-error-function-in-c
-//and https://stackoverflow.com/questions/60472139/computing-the-inverse-of-the-complementary-error-function-erfcinv-in-c
 //Credit goes to the author, njuffa, for this and the next function.
 float my_erfcinvf (float a)
 {
@@ -86,23 +84,30 @@ float my_normcdfinvf (float a)
     return fmaf (-1.41421356f, my_erfcinvf (a + a), 0.0f);
 }
 
-void H_G(float* psi, float* psi2, unsigned int n){
+//Changes made:
+//Deinterlace real and imaginary
+//Skip some unecessary calculations
+//  (when psi is real, first iteration where b2 is 0, etc)
+//pre-allocate where possible
+//Mark as restricted where possible
+
+void H_G(float* __restrict psi, float* __restrict psi2, const unsigned int n){
   //Calculates psi2 += H_G @ psi
   //Take into account that complex vectors are twice as long
-  n += 1;
-  unsigned int N = (1 << n);
+  //n += 1;
+  const unsigned int N = (1 << n);
 
   Vec16f a;
   Vec16f b;
   int h = 16;
+  constexpr int max_cache = 18;
 
   //Use permutations for h<16 cases
   for (int i = 0; i<N; i+=16){
     a.load(psi+i);
     b.load(psi2+i);
-    //Uncomment next line if using real arrays rather than complex
-    //b += permute16<1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14>(a);
 
+    b += permute16<1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14>(a);
     b += permute16<2, 3, 0, 1, 6, 7, 4, 5, 10, 11, 8, 9, 14, 15, 12, 13>(a);
     b += permute16<4, 5, 6, 7, 0, 1, 2, 3, 12, 13, 14, 15, 8, 9, 10, 11>(a);
     b += permute16<8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7>(a);
@@ -112,7 +117,7 @@ void H_G(float* psi, float* psi2, unsigned int n){
   //The vector can be kept in cache for these sizes
   //For now, this needs changing manually
   if (n > 4){
-    int end = N < 1 << 18 ? N : 1 << 18;
+    int end = N < (1 << max_cache) ? N : 1 << max_cache;
     for (int i = 0; i<N; i+=16){
       b.load(psi2+i);
       for (h = 16; h < end; h*=2){
@@ -125,10 +130,10 @@ void H_G(float* psi, float* psi2, unsigned int n){
   }
 
   //Cache can't save us so we have to suffer
-  if (n > 18){
+  if (n > max_cache){
     for (int i = 0; i<N; i+=16){
       b.load(psi2+i);
-      for (h = 1 << 18; h < N; h*=2){
+      for (h = 1 << max_cache; h < N; h*=2){
         int temp = h&i ? -h : h;
         a.load(psi + i + temp);
         b += a;
@@ -139,7 +144,7 @@ void H_G(float* psi, float* psi2, unsigned int n){
   return;
 }
 
-ArrayXcf Cheb(unsigned int n, float scale){
+ArrayXf Cheb(unsigned int n, float scale){
   //Returns n'th order Chebyshev expansion of exp(i*x*scale) on [-1,1]
   //Uses an FFT to calculate expansions for sin and cos separately, and sum them
   const std::complex<float> I(0.0,1.0);
@@ -150,7 +155,7 @@ ArrayXcf Cheb(unsigned int n, float scale){
   VectorXcf res1(2*n);
   VectorXcf res2(2*n);
 
-  VectorXcf out(n);
+  VectorXf out(n);
   for (int i = 0; i < n; i++){
     float temp = (2*i + 1)*PI/2/n;
     c[i] = cos(cos(temp)*scale);
@@ -163,44 +168,82 @@ ArrayXcf Cheb(unsigned int n, float scale){
 
   for (int i = 0; i < n; i++){
     float temp = PI*i/2/n;
-    out[i] = 4*real(exp(I*temp)*res1[i]) + 4.0f*I*real(exp(I*temp)*res2[i]);
+    out[i] = 4*real(exp(I*temp)*res1[i]) + 4.0f*real(exp(I*temp)*res2[i]);
   }
   out[0] /= 2.0f;
   return out;
 }
 
-ArrayXcf Clenshaw(VectorXcf coeffs, ArrayXcf psi, ArrayXf H_P, float gamma, float scale){
-  //Calculate exp(-i*H*t)@psi using the Clenshaw algorithm to evaluate a polynomial approximation
-  //Uses only matrix-vector multiplications
-  int n = coeffs.size();
-  ArrayXcf b1 = coeffs[n-1]*psi;
-  std::complex<float>* b1p = b1.data();
+ArrayXf Clenshaw(Eigen::Ref<VectorXf> coeffs,
+                  Eigen::Ref<ArrayXf> psi,
+                  Eigen::Ref<ArrayXf> H_P,
+                  float gamma,
+                  float scale, bool psi_real = false) {
+  // Calculate exp(-i*H*t)@psi using the Clenshaw algorithm
+  // with polynomial coefficients stored in coeffs.
 
-  ArrayXcf b2 = b1*0;
-  std::complex<float>* b2p = b2.data();
+  int N = H_P.size();
+  int n = log2(H_P.size());
+  thread_local ArrayXf b1, b2;
+  b2.setZero(2*N);
+  b1.setZero(2*N);
 
-  for (int r = n-2; r > 0; r--){
-    while (abs(coeffs[r]) < 1e-6) r--;
-    //The performance of this section seems very sensitive to the order of operations
-    //We do some trickery to calculate the next iteration in b2 without a temporary array
-    b2 *= (0.5*scale/gamma);
-    H_G((float*)b1p, (float*)b2p, log2(b1.size()));
-    b2 = (2/scale)*(H_P*b1 - gamma*b2);
-    b2 += psi*coeffs[r];
+  //Our arrays are N real values and then N imaginary values
+  //Since H_P and coeffs are real valued, we can skip unnecessary calculations
+  auto Re = [&](auto& x) -> decltype(auto) { return x.head(N); };
+  auto Im = [&](auto& x) -> decltype(auto) { return x.tail(N); };
 
-    //We essentially swap the labels b1 and b2 
-    Eigen::ArrayXcf &ref1 = b1;
-    Eigen::ArrayXcf &ref2 = b2;
-    std::swap(ref1, ref2);
-    b1p = b1.data();
-    b2p = b2.data();
+  float scale_fac = 0.5f * scale / gamma;
+  float scale_inv = 2.0f / scale;
+  bool first = true;
+  bool second = false;
+  for (int r = coeffs.size() - 1; r > 0; --r) {
+    while (abs(coeffs[r]) < 1e-6 && r > 0) --r;
+
+    if (not first){
+      // Scale b2 by (0.5 * scale / gamma)
+      b2 *= scale_fac;
+
+      // Apply H_G to (b1r,b1i) -> (b2r,b2i)
+      H_G(b1.data(), b2.data(), n);
+      H_G(b1.data()+N, b2.data()+N, n);
+
+      // Compute b2 = (2/scale) * (H_P*b1 - gamma*b2)
+      Re(b2) = scale_inv * (H_P * Re(b1) - gamma * Re(b2));
+      Im(b2) = scale_inv * (H_P * Im(b1) - gamma * Im(b2));
+    }
+
+    //Odd terms are imaginary
+    if (r&1){
+      if (not psi_real){b2.head(N) -= Im(psi)*coeffs[r];}
+      Im(b2) += Re(psi)*coeffs[r];
+    }
+    else{
+      if (not psi_real){Im(b2) += Im(psi)*coeffs[r];}
+      Re(b2) += Re(psi)*coeffs[r];
+    }
+    second = first;
+    first = false;
+    // Swap b1 and b2 without actually copying
+    std::swap(b1, b2);
   }
-  b2 *= (scale/gamma);
-  H_G((float*)b1p, (float*)b2p, log2(b1.size()));
-  b2 = (1/scale)*(H_P*b1 - gamma*b2) + psi*coeffs[0];
+
+  // Final iteration
+  float c_final = scale / gamma;
+  b2 *= c_final;
+
+  H_G(b1.data(), b2.data(), n);
+  H_G(b1.data()+N, b2.data()+N, n);
+
+  float s_final = 1.0f / scale;
+  Re(b2) = s_final * (H_P * Re(b1) - gamma * Re(b2));
+  Im(b2) = s_final * (H_P * Im(b1) - gamma * Im(b2));
+
+  b2 += psi * coeffs[0];
 
   return b2;
 }
+
 
 float heur[20] = {1.2082979794574937, 1.3131560483482256, 1.4100589067449547, 1.5006739957742985, 1.5861415036770354, 1.6672845086916428, 1.7447219000349252, 1.81893401086401, 1.8903031784517639, 1.9591400720339947, 2.025701482876633, 2.090202753583327, 2.1528267101788385, 2.2137302372981815, 2.2730492199794945, 2.330902325676296, 2.3873939451016604, 2.4426165114475493, 2.4966523525078355, 2.5495751865531977};
 
@@ -214,24 +257,21 @@ int main(int argc, char* argv[]){
   
   char* filename = argv[3];
 
-  //Which entry to start and how many to do
   unsigned int start = 0;
-  unsigned int reps = 2000;
+  unsigned int problems = 2000;
 
-  float* results = (float*)malloc(reps*4);
-  float* gaps = (float*)malloc(reps*4);
-
+  float* results = (float*)malloc(problems*4);
 
   //Number of samples for Monte-Carlo integral
   int samples = 100;
 
   if (argc == 6){
     start = atoi(argv[4]);
-    reps = atoi(argv[5]);
+    problems = atoi(argv[5]);
   }
 
   char output[20];
-  std::sprintf(output, "output_%d_%d_%d", n, m, start);
+  std::sprintf(output, "output_%d_%d", n, m);
   std::ofstream outFile(output, std::ios::binary | std::ios::app);
 
   std::ifstream file(filename, std::ios::binary);
@@ -240,33 +280,23 @@ int main(int argc, char* argv[]){
   file.seekg(4*(n+1)*n*start, file.beg);
 
   Eigen::ArrayXXf times(m,samples);
-  Eigen::ArrayXf probs(samples);
+  Eigen::ArrayXf success_probabilities(samples);
 
   ArrayXf gammas(m);
   ArrayXf onenorms(m);
 
-  //Problem energy levels
   ArrayXf H_P(N);
+  ArrayXf psi(2*N);
 
-  //This is used for calculating H_P, state(i,j) = sigma_z_i * sigma_z_j
-  ArrayXXf state(n,n);
-
-  //Our quantum register
-  ArrayXcf psi(N);
-
-  //Ising problem parameters
   ArrayXXf J(n,n);
-  ArrayXf h(n);
-
-  //Used for reading in parameters
+  ArrayXXf state(n,n);
+  
   char buffer[4*n*(n+1)];
   double temp[n*(n+1)/2];
 
-  for (int rep = 0; rep < reps; rep++){
+  for (int problem = 0; problem < problems; problem++){
 
-    //We care about both the ground state and the energy gap
     double E_0 = 0;
-    double E_1 = 0;
     double E_max = 0;
     unsigned int E_loc = 0;
 
@@ -284,26 +314,19 @@ int main(int argc, char* argv[]){
         k++;
       }
     }
-
-    //Absorb 1/2 factor into J to follow paper
     J += J.transpose().eval();
-    J /= 2;
 
     for (int i = 0; i < n; i++){
-      h(i) = temp[n*(n-1)/2 + i];
+      J(i,i) = 2*temp[n*(n-1)/2 + i];
     }
 
     state.setConstant(1);
-    psi.setConstant(1/sqrt(N));
+    psi.head(N).setConstant(1/sqrt(N));
+    psi.tail(N).setZero();
 
-    //The way we calculate E is prone to error so use a double
-    //Start with the energy of the all -1s state
-    double E = J.sum() - h.sum();
+    //The way we calculate is prone to error so use a double
+    double E = J.sum()/2;
     H_P[0] = E;
-
-    //In case first state is ground
-    E_0 = E;
-    E_max = E;
   
     //Use a grey code to efficiently evaluate all energies
     for (unsigned int i = 1; i < N; i++){
@@ -311,17 +334,13 @@ int main(int argc, char* argv[]){
       state.row(flip) *= -1;
       state.col(flip) *= -1;
       state(flip,flip) *= -1;
-      E += 4*(J.row(flip)*state.row(flip)).sum() - 2*h(flip)*state(flip,flip);
+      E += (2*(J.row(flip)*state.row(flip)).sum() - J(flip,flip)*state(flip,flip));
       H_P[grey(i)] = E;
 
       //keep track of ground state
       if (E < E_0){
-        E_1 = E_0;
         E_0 = E;
         E_loc = grey(i);
-      }
-      else if (E < E_1){
-        E_1 = E;
       }
 
       //keep track of highest state too
@@ -330,38 +349,27 @@ int main(int argc, char* argv[]){
       }
     }
     //We want to shift H_P to reduce the spectral radius
-    //float kurt = (H_P*H_P*H_P*H_P).mean() / pow((H_P*H_P).mean(),2);
-    //float skew = (H_P*H_P*H_P).mean() / pow((H_P*H_P).mean(),1.5);
+    //This doesn't change the result but shortens the calculation
+    float kurt = (H_P*H_P*H_P*H_P).mean() / pow((H_P*H_P).mean(),2);
+    float skew = (H_P*H_P*H_P).mean() / pow((H_P*H_P).mean(),1.5);
     H_P -= (E_max + E_0)/2;
     float E_abs = (E_max - E_0)/2;
     //Now H_P has been calculated
 
-    // This formula is from https://math.stackexchange.com/questions/89030/expectation-of-the-maximum-of-gaussian-random-variables/89147#89147
-    //Calculates estimated maximum energy level using the known variance and assuming a normal distribution
     float b = my_normcdfinvf(1/(float)N);
     float e_m = 0.577215664901532860;
     float e = 2.718281828459045;
     float a = (1 - e_m)*b + e_m*my_normcdfinvf(1/(e*N));
-
-    //If the h was 0, we'd just want HP2 = 2*(J*J).sum()
-    //If we were to map to an n+1 qubit problem we'd get h/2 in both a row and a column
-    //so we need to add 2*2*(h/2).dot(h/2) = h.dot(h) 
-    float HP2 = 2*(J*J).sum() + (h*h).sum();
-
+    float HP2 = (2*(J*J).sum() - (J.matrix().diagonal().dot(J.matrix().diagonal())))/4;
     //std::cout << (E_abs - a*sqrt(HP2))/E_abs << " " << kurt << " " << sqrt(HP2)*(my_normcdfinvf(1/(e*N)) - b)*sqrt(PI*PI/6)/E_abs << "\n";
-    std::cout << HP2 << " " << E_0 << "\n";
+    //std::cout << (H_P * H_P).sum()/N << " " << HP2 << " " << heur[n - 5]*n << " " << E_0 << "\n\n";
   
-    //Calculate walk time
-
-    //Similar to above, we want 8*(J*J).sum(), but having h != 0 changes the dynamics
-    //We have to derive a new formula that works for h != 0, as mapping to n+1 qubits also changes the dynamics
-    //The new formula is 8*(J*J).sum() + 2*h.h
-    float short_t = 8*(J*J).sum() + 2*(h*h).sum();
+    //Calculate ideal short time
+    float short_t = 2*(J*J).sum() - 1.5*(J.matrix().diagonal().dot(J.matrix().diagonal()));
     short_t = sqrt(2*n/short_t);
 
-    //Should really use higher quality RNG
     std::random_device rd;
-    std::mt19937 gen(rd());
+    std::mt19937 gen(123);
     std::uniform_real_distribution<float> rand_t(short_t, 2*short_t);
 
     //Calculate gammas, upper bounds on spectral radius and generate evolution times.
@@ -376,21 +384,16 @@ int main(int argc, char* argv[]){
       }
 
     }
+    std::sort(gammas.begin(), gammas.end(), std::greater<float>());
 
     //Optimisation for single-stage
     if (m == 1){
       std::sort(times.row(0).begin(), times.row(0).end());
       //Have to calculate backwards to avoid modifying data that's still needed
-      times(0,seq(last,1,-1)) -= times(0,seq(last-1,0,-1));
+      times(0,seq(placeholders::last,1,-1)) -= times(0,seq(placeholders::last-1,0,-1));
     }
 
     for (int j = 0; j < samples; j++){
-      //Uncomment this section to randomly sample gamma values
-      /*for (int i = 0; i<m; i++){
-        gammas[i] = heur[n - 5]/tan(rand_g(gen));
-      }
-      std::sort(gammas.begin(), gammas.end(), std::greater<float>());
-      */
 
       //Loop through all the times and calculate the success probability
       for (int i = 0; i < m; i++){
@@ -400,22 +403,23 @@ int main(int argc, char* argv[]){
         //So for stages with large gamma, H_P is predicted to saturate first.
 	if (1.0/tan(PI*(i+1)/(2*m + 2)) > 1) times(i,j) /= sqrt(1.0/tan(PI*(i+1)/(2*m + 2)));
         int terms = 32 + (int)onenorm*times(i,j);
-        ArrayXcf coeffs = Cheb(terms,onenorm*times(i,j));
+        ArrayXf coeffs = Cheb(terms,onenorm*times(i,j));
         while (abs(coeffs[terms - 1]) > 1e-6){
           terms*=2;
           coeffs = Cheb(terms, onenorm*times(i,j));
         }
-
-        psi = Clenshaw(coeffs, psi, H_P, gamma, onenorm);
+        psi = Clenshaw(coeffs, psi, H_P, gamma, onenorm, j == 0);
       }
-      probs(j) = abs(psi[E_loc])*abs(psi[E_loc]);
-      if (m != 1) psi.setConstant(1/sqrt(N));
+      success_probabilities(j) = psi[E_loc]*psi[E_loc] + psi[E_loc+N]*psi[E_loc+N];
+
+      if (m != 1){
+        psi.head(N).setConstant(1/sqrt(N));
+        psi.tail(N).setZero();
+      }
 
     }
-  results[rep] = probs.sum()/samples;
-  std::cout << results[rep] << "\n\n";
-  //gaps[rep] = E_1 - E_0;
+  results[problem] = success_probabilities.sum()/samples;
+  std::cout << results[problem] << "\n";
   }
-  outFile.write(reinterpret_cast<const char*>(results), reps * sizeof(float));
-  //outFile.write(reinterpret_cast<const char*>(gaps), reps * sizeof(float));
+  outFile.write(reinterpret_cast<const char*>(results), problems * sizeof(float));
 }
